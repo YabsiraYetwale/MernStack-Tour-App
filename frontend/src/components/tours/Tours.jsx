ab79d82b38e3aff7869989b8cea70e08c1a5a38c
YabsiraYetwale <yabsirayetwale@gmail.com>
2024-03-08 14:45:02 +0300

    updated

--- frontend/src/components/tours/Tours.jsx
+++ frontend/src/components/tours/Tours.jsx
@@ -1,12 +1,11 @@
 import { useSelector } from 'react-redux'
-// import {tours} from '../utils'
 import Tour from './tour/Tour'
 const Tours =()=> {
     const {tours}=useSelector(state=>state.tours)
     return(
         <>
          <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexWrap:'wrap',gap:'2rem'}}>
-            {tours.map((tour,i)=>(
+            {tours?.map((tour,i)=>(
                 <div key={i}>
                     <Tour tour={tour}/>
                 </div>
--- frontend/src/pages/Detail.jsx
+++ frontend/src/pages/Detail.jsx
@@ -1,37 +1,46 @@
-import {tours} from '../components/utils'
-import {Link} from 'react-router-dom'
+// import {tours} from '../components/utils'
+import {Link, useParams} from 'react-router-dom'
 import Comment from '../components/comments/Comment'
 import RelatedTours from '../components/relatedTours/RelatedTours'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { fetchTour } from '../redux/actions/tours'
+import { img_url } from '../redux/api'
 const Detail =()=> {
+    const {id}=useParams()
+    const dispatch=useDispatch()
+    const {isLoading,tour}=useSelector(state=>state.tours)
+    useEffect(()=>{
+        dispatch(fetchTour(id))
+    },[id,dispatch])
     return(
         <>
         <div style={{display:'flex',
            flexDirection:'column',
            position:'relative',borderRadius:'7px',
            padding:'0 40px 20px 40px',}}>
-       { tours.slice(1,2).map((tour,i)=>(
-           <div key={i} style={{display:'flex',
+           <div  style={{display:'flex',
            flexDirection:'column',
            }}>
-           <Link to={`/tour/${tour._id}`}/><img src={tour.image} alt='image' style={{width:'100%',height:'380px',borderTopLeftRadius:'7px',borderTopRightRadius:'7px'}}/>
+           <Link to={`/tour/${tour?._id}`}/><img src={`${img_url}${tour?.image}`} alt='image' style={{width:'100%',height:'380px',borderTopLeftRadius:'7px',borderTopRightRadius:'7px'}}/>
             <div style={{padding:'0px 0px 40px 20px',}}>
             <div style={{fontWeight:'bolder',fontSize:'25px',
             padding:'30px 0px 10px 0px',textAlign:'center',
            }}>
-            {tour.title}</div>
-            <div style={{fontWeight:'bolder'}}>Created By: {tour.creator}</div>
+            {tour?.title}</div>
+            <div style={{fontWeight:'bolder'}}>Created By: {tour?.creator}</div>
                 <div style={{display:'flex',gap:'8px'}}>
-                    {tour.tags.map((tag,i)=>(
+                    {tour?.tags?.map((tag,i)=>(
                     <div key={i}>
                         #{tag}
                     </div>
                 ))}
                 </div>
-                <div>{tour.likes} Likes</div>
+                <div>{tour?.likes} Likes</div>
                 <div>2 months ago</div>
-            <div>{tour.description}</div>
+            <div>{tour?.description}</div>
             </div>
-        </div>))}
+        </div>
         <RelatedTours/>
        <Comment/>
         </div>
